ede9df0e8ac1caa131113fe7a6746bf663275048
Philippe Caron <philippe-caron@hotmail.com>
2025-08-23 15:05:26 -0400

    remove intellisense hack

--- inc/diagnostic.export.h
+++ inc/diagnostic.export.h
@@ -8,9 +8,6 @@
 #else
 #define DIAGNOSTIC_EXPORT __declspec(dllimport)
 #endif
-#ifdef __INTELLISENSE__
-#define __VA_OPT__(X)
-#endif
 #else
 #define DIAGNOSTIC_EXPORT __attribute__((visibility("default")))
 #endif
